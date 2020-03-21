eeec842d2520ccd0725b7fc9d14ae3101ad9341d
Bryson Tanner <realredpatriot@gmail.com>
2020-03-20 22:32:17 -0600

    Added functions to namespace eng. Updated documentation. Updated naming schema. Added declarations of Planar Stress analysis

--- EngineeringLibrary/MaterialStress.h
+++ EngineeringLibrary/MaterialStress.h
@@ -8,39 +8,57 @@
 #include "PhysicsLibrary\Pressure.h"
 #include "PhysicsLibrary\Area.h"
 #include "PhysicsLibrary\Length.h"
+#include "PhysicsLibrary/Angle.h"
 
 /* MaterialsStress.h
  * Functions to calculate the stresses in materials under different loading conditions
  */
 
-namespace stress {
+namespace eng {
 
-  /* Stress in cylindrical pressure vessels */
-  namespace cylindrical {
-    /* The stresses at one stress element in a member */
-    struct ENGINEERINGLIBRARY_API StressState {
-      eng::Stress s_t;
-      eng::Stress s_r;
-      eng::Stress s_z;
+  /* ***********************************************************************************************
+   * 2D (Planar) Stress analysis
+   */
 
-      StressState(const eng::Stress& tt = 0_Pa, const eng::Stress& rr = 0_Pa, const eng::Stress& zz = 0_Pa);
-    };
+  /* The stress of one 2 Dimensional stress element in a member */
+  struct StressPlanar {
+    Stress sigma_x;
+    Stress sigma_y;
+    Stress tau_xy;
+    physics::Angle theta;
 
-    /* Calculate the stress in a Cylindrical pressure vessel. */
-    StressState ENGINEERINGLIBRARY_API principal_stress(const physics::Length& a, const physics::Length& b, 
-      const physics::Length& r, const physics::Force& F, const physics::Pressure& Pi, 
-      const physics::Pressure& Po = 0_kPa);
+    StressPlanar(const Stress& xx = 0_Pa, const Stress& yy = 0_Pa, const Stress& xxyy = 0_Pa, 
+      const physics::Angle& tt = 0_rad);
+  };
 
-    /* Calculate the interference of two press-fit cylinders. */
-    physics::Length ENGINEERINGLIBRARY_API press_fit_interference(const physics::Length& ri,
-      const physics::Length& R, const physics::Length& ro, const physics::Pressure& p,
-      const eng::MaterialBase& out_material, const eng::MaterialBase& in_material);
+  /* Calculate ths principal stress of a planar stress state */
+  StressPlanar principal_stress(const StressPlanar& s);
+  StressPlanar maximum_shear(const StressPlanar& s);
 
-  };  // namespace cylindrical
+  /* ***********************************************************************************************
+   * Cylindrical Pressure Vessel Stress analysis
+   */
 
-  // TODO: Write a method for general 2D stress states.
+  /* The stresses at one stress element in a cylindrical pressure vessel */
+  struct ENGINEERINGLIBRARY_API StressCylindrical {
+    Stress sigma_t;
+    Stress sigma_r;
+    Stress sigma_z;
 
-}; // namespace stress
+    StressCylindrical(const Stress& tt = 0_Pa, const Stress& rr = 0_Pa, const Stress& zz = 0_Pa);
+  };
+
+  /* Calculate the stress in a Cylindrical pressure vessel. */
+  StressCylindrical ENGINEERINGLIBRARY_API principal_stress(const physics::Length& a, const physics::Length& b, 
+    const physics::Length& r, const physics::Force& F, const physics::Pressure& Pi, 
+    const physics::Pressure& Po = 0_kPa);
+
+  /* Calculate the interference of two press-fit cylinders. */
+  physics::Length ENGINEERINGLIBRARY_API press_fit_interference(const physics::Length& ri,
+    const physics::Length& R, const physics::Length& ro, const physics::Pressure& p,
+    const MaterialBase& out_material, const MaterialBase& in_material);
+
+}; // namespace eng
 
 
 #endif
